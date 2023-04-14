9010342e07af05eb096e94006cb9fc4059466b75
Rasoul Norouzi <46104045+rasoulnorouzi@users.noreply.github.com>
2023-04-14 14:16:25 +0200

    I added causal sentence extractror!

--- src/tools/CausalSentenceExtractor.py
+++ src/tools/CausalSentenceExtractor.py
@@ -1,1 +1,26 @@
-"Nothing to see here yet."
+class causalityExtractor:
+    def __init__(
+                self, model, tokenizer, threshold=0.9, batch_size=32):
+        
+        self.model = model
+        self.tokenizer = tokenizer
+        self.threshold = threshold
+        self.batch_size = batch_size
+
+
+    def batch_generator(self, sentences):
+        for i in range(0, len(sentences), self.batch_size):
+            yield sentences[i:i+self.batch_size]
+
+
+    def forward(self, sentences):
+        causal_sentence = []
+        for batch in self.batch_generator(sentences):
+            inputs = self.tokenizer(batch, return_tensors='pt', padding=True, truncation=True)
+            outputs = self.model(**inputs)
+            predicted_labels = [1 if outputs.logits[i][1] > self.threshold else 0 for i in range(len(outputs.logits))]
+            
+            for i, label in enumerate(predicted_labels):
+                if label == 1:
+                    causal_sentence.append(batch[i])
+        return causal_sentence
